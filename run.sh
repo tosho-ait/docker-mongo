1fdbc9194409bf92b6ad6460b58aaca58d55210a
Tosho Ivanov <todor.ait@gmail.com>
2018-05-07 18:34:24 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -m
  
-mongodb_cmd="mongod --storageEngine $STORAGE_ENGINE"
-cmd="$mongodb_cmd --httpinterface --rest --master"
+cmd="mongod --storageEngine $STORAGE_ENGINE --rest --master"
+
 if [ "$AUTH" == "yes" ]; then
     cmd="$cmd --auth"
 fi
